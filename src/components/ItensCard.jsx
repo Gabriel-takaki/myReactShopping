f8f905d5394b8f67c52c7c8e4d371849675732ec
Gabriel Takaki <gabriel.f.takaki@gmail.com>
2021-12-25 15:41:12 -0300

    initial commit

---
+++ src/components/ItensCard.jsx
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import carrinho from '../imagens/carrinho.png'
+import '../styles/search.css'
+
+function ItensCard({ itensName, itensImage, itensPrice, }) {
+
+  return (
+    <div className='itens-div'>
+      <img className='iten-image' src={ itensImage } alt={ itensName } />
+      <h4 className='iten-name'>{ itensName }</h4>
+      <h5 className='iten-price'> R$ {itensPrice} </h5>
+      <button className='add-car-button'>
+        <img className='add-car-button-img' src={carrinho} alt="" />
+      </button>
+    </div>
+  );
+}
+
+ItensCard.propTypes = {
+  itensImage: PropTypes.string.isRequired,
+  itensName: PropTypes.string.isRequired,
+  index: PropTypes.number.isRequired,
+};
+
+export default ItensCard;
